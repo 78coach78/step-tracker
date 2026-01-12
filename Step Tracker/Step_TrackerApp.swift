be2de7c0f81ad7bea5e02b0cda086dc718a49162
78coach78 <info@coach78.com>
2026-01-12 20:56:01 +0100

    created xcode project

---
+++ Step Tracker/Step_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_TrackerApp.swift
+//  Step Tracker
+//
+//  Created by Roland Ramseier on 12.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct Step_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
