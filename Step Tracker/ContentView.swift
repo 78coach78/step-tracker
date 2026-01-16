325b1864ee8cf9a726583fd4dec56fe086152c29
78coach78 <info@coach78.com>
2026-01-16 13:40:51 +0100

    initial dashboard ui

--- Step Tracker/ContentView.swift
+++ Step Tracker/ContentView.swift
@@ -11,30 +11,61 @@ struct ContentView: View {
     var body: some View {
         NavigationStack{
             ScrollView{
-                VStack {
+                VStack(spacing: 20) {
                     VStack {
-                        HStack {
-                            VStack(alignment: .leading) {
-                                Label("Steps", systemImage: "figure.walk")
-                                    .font(.title3.bold())
-                                    .foregroundStyle(.pink)
+                        VStack {
+                            HStack {
+                                VStack(alignment: .leading) {
+                                    Label("Steps", systemImage: "figure.walk")
+                                        .font(.title3.bold())
+                                        .foregroundStyle(.pink)
+                                    
+                                    Text("Avg: 10k Steps")
+                                        .font(.caption)
+                                        .foregroundColor(.secondary)
+                                }
                                 
-                                Text("Avg: 10k Steps")
-                                    .font(.caption)
-                                    .foregroundColor(.secondary)
+                                Spacer()
+                                
+                                Image(systemName: "chevron.right")
+                                    .foregroundStyle(.secondary)
                             }
+                            .padding(.bottom, 12)
                             
-                            Spacer()
-                            
-                            Image(systemName: "chevron.right")
+                            RoundedRectangle(cornerRadius: 12)
                                 .foregroundStyle(.secondary)
+                                .frame(height: 150)
+                            
                         }
+                        .padding()
+                        .background(RoundedRectangle(cornerRadius: 12).fill(Color(.secondarySystemBackground)))
+                        
+                        VStack(alignment: .leading) {
+                                VStack(alignment: .leading) {
+                                    Label("Averiges", systemImage: "calendar")
+                                        .font(.title3.bold())
+                                        .foregroundStyle(.pink)
+                                    
+                                    Text("Last 28 Days")
+                                        .font(.caption)
+                                        .foregroundColor(.secondary)
+                                }
+                            .padding(.bottom, 12)
+                            
+                            RoundedRectangle(cornerRadius: 12)
+                                .foregroundStyle(.secondary)
+                                .frame(height: 240)
+                            
+                        }
+                        .padding()
+                        .background(RoundedRectangle(cornerRadius: 12).fill(Color(.secondarySystemBackground)))
+                        
                     }
+                    
                 }
                 .padding()
-        }
-            .padding()
-            .navigationTitle("Dashboard")
+                .navigationTitle("Dashboard")
+            }
         }
     }
 }
