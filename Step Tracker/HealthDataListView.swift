8b268b80324335eddf1cc039b62475ae1cef7448
78coach78 <info@coach78.com>
2026-01-24 19:35:24 +0100

    Update HealthDataListView.swift
    2nd change

--- Step Tracker/HealthDataListView.swift
+++ Step Tracker/HealthDataListView.swift
@@ -9,20 +9,62 @@ import SwiftUI
 
 struct HealthDataListView: View {
     
+    @State private var isShowingAddData = false
+    
+    @State private var adddDataDate: Date = .now
+    @State private var valueToAdd: String = ""
+    
     var metric: HealthMetricContext
     
     var body: some View {
         List(0..<28) { i in
-        HStack {
-            Text(Date(), format: .dateTime.month().day().year())
-            Spacer()
-            Text(10000, format: .number.precision(.fractionLength(metric == .steps ? 0 : 1)))
+            HStack {
+                Text(Date(), format: .dateTime.month().day().year())
+                Spacer()
+                Text(10000, format: .number.precision(.fractionLength(metric == .steps ? 0 : 1)))
             }
         }
         .navigationTitle(Text(metric.title))
+        .sheet(isPresented: $isShowingAddData) {
+            addDataView
+        }
+        .toolbar {
+            Button("Add Data", systemImage: "plus") {
+                isShowingAddData = true
+            }
+        }
     }
-}
-
+    
+    var addDataView: some View {
+        NavigationStack {
+            Form {
+                DatePicker("Date", selection: $adddDataDate, displayedComponents: .date)
+                TextField("Value", text: $valueToAdd)
+            }
+            .navigationTitle(metric.title)
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("Add Data") {
+                        //Do later
+                    }
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button("dismiss") {
+                        //Do later
+                    }
+                    
+                }
+            }
+            
+           
+                }
+            }
+        }
+    
 #Preview {
-    HealthDataListView(metric: .steps)
+    NavigationStack {
+        HealthDataListView(metric: .steps)
+    }
 }
