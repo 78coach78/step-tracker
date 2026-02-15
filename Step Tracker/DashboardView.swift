6229150ce48740cac2d664951ad20102b3c551e8
78coach78 <info@coach78.com>
2026-02-15 10:04:55 +0100

    mock data on simulator

--- Step Tracker/DashboardView.swift
+++ Step Tracker/DashboardView.swift
@@ -23,6 +23,7 @@ enum HealthMetricContext: CaseIterable, Identifiable {
 
 struct DashboardView: View {
     
+    @Environment(HealthKitManager.self) private var hkManager
     @AppStorage("hasSeenPermissionPriming") private var hasSeenPermissionPriming = false
     @State private var isShowingPermissionPrimingSheet: Bool = false
     @State private var selectedStat: HealthMetricContext = .steps
@@ -68,15 +69,15 @@ struct DashboardView: View {
                         .background(RoundedRectangle(cornerRadius: 12).fill(Color(.secondarySystemBackground)))
                         
                         VStack(alignment: .leading) {
-                                VStack(alignment: .leading) {
-                                    Label("Averiges", systemImage: "calendar")
-                                        .font(.title3.bold())
-                                        .foregroundStyle(.pink)
-                                    
-                                    Text("Last 28 Days")
-                                        .font(.caption)
-                                        .foregroundColor(.secondary)
-                                }
+                            VStack(alignment: .leading) {
+                                Label("Averiges", systemImage: "calendar")
+                                    .font(.title3.bold())
+                                    .foregroundStyle(.pink)
+                                
+                                Text("Last 28 Days")
+                                    .font(.caption)
+                                    .foregroundColor(.secondary)
+                            }
                             .padding(.bottom, 12)
                             
                             RoundedRectangle(cornerRadius: 12)
@@ -91,24 +92,25 @@ struct DashboardView: View {
                     
                 }
                 .padding()
-                .onAppear {
-                isShowingPermissionPrimingSheet = !hasSeenPermissionPriming
+                .task {
+                    isShowingPermissionPrimingSheet = !hasSeenPermissionPriming
                 }
-                .navigationTitle("Dashboard")
-                .navigationDestination(for: HealthMetricContext.self) {metric in HealthDataListView(metric: metric)
-                }
-                .sheet(isPresented: $isShowingPermissionPrimingSheet, onDismiss: {
-                    // fetch health data
-                }, content: {
-                    HealthKitPermissionPrimingView(hasSeen: $hasSeenPermissionPriming)
-                })
             }
+            .navigationTitle("Dashboard")
+            .navigationDestination(for: HealthMetricContext.self) {metric in HealthDataListView(metric: metric)
+            }
+            .sheet(isPresented: $isShowingPermissionPrimingSheet, onDismiss: {
+                // fetch health data
+            }, content: {
+                HealthKitPermissionPrimingView(hasSeen: $hasSeenPermissionPriming)
+            })
         }
         .tint(isSteps ? .pink : .indigo)
     }
 }
+    
+    #Preview {
+        DashboardView()
+            .environment(HealthKitManager())
+    }
 
-#Preview {
-    DashboardView()
-        .environment(HealthKitManager())
-}
