7a4b80fa81c2e66adbd25ade2a69ac16fb4cc6e0
78coach78 <info@coach78.com>
2026-02-19 10:52:03 +0100

    step bar chart styling

--- Step Tracker/Healthmetric.swift
+++ Step Tracker/Healthmetric.swift
@@ -11,4 +11,18 @@ struct Healthmetric: Identifiable {
     let id = UUID()
     let date: Date
     let value: Double
+    
+    static var mockData: [Healthmetric] {
+        var array: [Healthmetric] = []
+        for i in 0..<28 {
+            let metric = Healthmetric(date: Calendar.current.date(byAdding: .day, value: -i, to: .now)!, value: .random(in: 4000...15000))
+            
+            array.append(metric)
+            
+        }
+        return array
+    }
 }
+
+
+
