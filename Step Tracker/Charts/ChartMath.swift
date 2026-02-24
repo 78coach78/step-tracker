be24c332f410bfacbd59b5dc8e067cd3ffcea73d
78coach78 <info@coach78.com>
2026-02-24 17:28:49 +0100

    Avg Step Chart calculations

--- Step Tracker/Charts/ChartMath.swift
+++ Step Tracker/Charts/ChartMath.swift
@@ -11,13 +11,27 @@ import Algorithms
 struct ChartMath {
     static func averageWeekdayCount(for metric: [Healthmetric]) -> [WeekdayChartData] {
         let sortedByWeekday = metric.sorted { $0.date.weekdayInt < $1.date.weekdayInt}
-        let weekdayArray = sortedByWeekday.chunked(by: <#T##(Healthmetric, Healthmetric) throws -> Bool#>)
+        let weekdayArray = sortedByWeekday.chunked { $0.date.weekdayInt == $1.date.weekdayInt }
         
+        var weekdayChartData: [WeekdayChartData] = []
         
-        for metric in sortedByWeekday {
-            print(metric.date.weekdayInt)
+        for array in weekdayArray {
+            guard let firstValue = array.first else { continue }
+            let total = array.reduce(0) { $0 + $1.value }
+            let avgSteps = total / Double(array.count)
+            
+            weekdayChartData.append(.init(date: firstValue.date, value: avgSteps))
         }
         
-        return []
+        for metric in sortedByWeekday {
+            print("Day: \(metric.date.weekdayInt), value: \(metric.value)")
+        }
+        
+        print("-----")
+        
+        for day in weekdayChartData {
+            print("Day: \(day.date.weekdayInt), value: \(day.value)")
+        }
+        return weekdayChartData
     }
 }
